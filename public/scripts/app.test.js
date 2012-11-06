d1eb910de690c2a3e08271549dc259c59e49c026
Philippe Breuils <philippe.breuils@gmail.com>
2012-11-06 13:29:48 +0100

    made servos work

--- public/scripts/app.test.js
+++ public/scripts/app.test.js
@@ -1,9 +1,8 @@
 var pl = 'scripts/libs/';
+var servo = null;
+var board = null;
+var Noduino = null;
 require(["jquery", pl + 'Noduino.js', pl + 'Noduino.Socket.js', pl + 'Logger.HTML.js'], function($, NoduinoObj, Connector, Logger) {
-  var Noduino = null;
-  var board = null;
-  var servo = null;
-  
   $(document).ready(function(e) {
     var host = $('#host').val();
     $('#servo-slide').change(function() {
@@ -13,12 +12,11 @@ require(["jquery", pl + 'Noduino.js', pl + 'Noduino.Socket.js', pl + 'Logger.HTM
     });
     if (!Noduino || !Noduino.connected) {
       Noduino = new NoduinoObj({debug: true, host: 'http://' + host + ':8090', logger: {container: '#connection-log'}}, Connector, Logger);
-      Noduino.connect(function(err, board) {
-        console.log("zob");
+      Noduino.connect(function(err, b) {
+        board = b;
         board.withServo({pin: 9, pos: 45}, function(err, s) {
           servo = s;
           servo.attach();
-          console.log(servo);
         });
       });
     }
--- public/scripts/libs/Servo.js
+++ public/scripts/libs/Servo.js
@@ -5,18 +5,26 @@ define(function() {
       return new Servo(options); }
 
     this.c      = Connector;
-    this.pin    = options.pin;
+    this.pin    = normalizePin(options.pin || 9);
     this.pos    = options.pos || 90;
   };
 
   Servo.prototype.write = function (pos, callback) {
-    this.c.current().write('98' + this.pin + '02' + pos);
+    this.c.current().write('98' + this.pin + '002' + normalizeVal(pos));
   }
 
   Servo.prototype.attach = function (callback) {
-    this.c.current().write('98' + this.pin + '01');
+    this.c.current().write('98' + this.pin + '001');
     this.write(this.pos, callback);
   }
   
+  function normalizePin(pin) {
+    return ("00" + pin).substr(-2);
+  }
+
+  function normalizeVal(val) {
+    return ("000" + val).substr(-3);
+  }
+
   return Servo;
 });
